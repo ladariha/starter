96f39533f2b69bf2825baef3cd2d5adb87bd25c1
atuse <mada.tran@gmail.com>
2025-02-18 09:19:26 +0100

    add event to react app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { Demo } from "./pages/Demo/Demo";
+import { Event } from "./pages/Event/Event";
+import { UserRecord, DateRecord } from "./pages/VoteResults/VoteResults";
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <Demo name="lada" />
-    </div>
-  )
-  ;
+
+    const userRecords: UserRecord[] = [{ name: "Adam", answer: "yes" },
+        { name: "Martan", answer: "no" },
+        { name: "Ulrich", answer: "no" }];
+    const dateRecords: DateRecord[] = [{ timestamp: "14.05.2024", record: userRecords },
+        { timestamp: "15.05.2024", record: userRecords },
+        { timestamp: "15.06.2024", record: userRecords }];
+    return (
+        <div>
+            <h1>Event planner</h1>
+            <Event title="Fasiangy" location="Bratislava" date={dateRecords}></Event>
+            <Event title="Den-D" location="Brno" date={dateRecords}></Event>
+            <Event title="Den-F" location="Brno" date={dateRecords}></Event>
+        </div>
+    );
 };
 
 export default App;
